f952993e841352048c1fdf5f2155c26b5bdb6029
Henry Campbell <hacampbell99@gmail.com>
2022-04-08 10:33:01 +1000

    Setup basic server

--- src/server/NetwordleGame.java
+++ src/server/NetwordleGame.java
@@ -1,5 +1,46 @@
 package src.server;
 
-public class NetwordleGame {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class NetwordleGame extends Thread{
+    private Socket client;
+    private PrintWriter writer;
+    private BufferedReader reader;
+    private String cAddress;
+
+    public NetwordleGame (Socket client) {
+        try {
+            this.client = client;
+            this.cAddress = client.getLocalSocketAddress().toString();
+
+            // Setup for Buffered Reader
+            InputStreamReader in = new InputStreamReader(client.getInputStream());
+            this.reader = new BufferedReader(in);
+
+            // Steup for Print Writer
+            this.writer = new PrintWriter(client.getOutputStream(), true);
+            
+        } catch (IOException e) {
+            System.out.println("Unable to create Networdle Client." + 
+                                "Error was:\n" + e.getMessage());
+            
+        }
+    }
+
+    public void run () {
+        try {
+            writer.println("Welcome to the server!");
+            writer.println("You are client: " + cAddress);
+
+        } catch (Exception e) {
+            System.out.println("An error occured for client at address: " + 
+                                cAddress);
+            System.out.println("Error was:\n" + e.getMessage());
+        }
+    }
+
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,11 +1,18 @@
 package src.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import src.shared.Utils;
+
 public class Server {
     private int port;
+    private ServerSocket ss;
 
     /**
      * Server constructor
-     * @param args - The command line arguments given to the program
+     * @param port - The port to start the server on
      */
     public Server (int port) {
         this.port = port;
@@ -15,6 +22,34 @@ public class Server {
      * Main driver function for the server
      */
     public void start () {
-        System.out.println("Starting server on port " + this.port);
+        try {
+            ss = new ServerSocket(this.port);
+
+            System.out.println("Server listening on port " + this.port);
+
+            // Main sever loop
+            while (true) {
+                Socket client = ss.accept();
+                NetwordleGame clientGame = new NetwordleGame(client);
+                clientGame.start();
+            }
+
+        } catch (IOException e) {
+            Utils.errorAndDie("Unable to create server. Please try again, " +
+                                "perhaps with another port.");
+        } finally {
+            closeServer();
+        }
+    }
+
+    /**
+     * A small ultilty function used for closing the server.
+     */
+    private void closeServer () {
+        try {
+            ss.close();
+        } catch (IOException e) {
+            Utils.errorAndDie("An error occured while closing the server");
+        }
     }
 }
