bb14b59f2e4248ac277df05066f87bb19df3e23c
Henry Campbell <hacampbell99@gmail.com>
2022-04-08 13:48:58 +1000

    Added method for selecting games target word

--- src/server/NetwordleGame.java
+++ src/server/NetwordleGame.java
@@ -1,16 +1,22 @@
 package src.server;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+import src.shared.Utils;
 
 public class NetwordleGame extends Thread{
     private Socket client;
     private PrintWriter writer;
     private BufferedReader reader;
     private String cAddress;
+    private String targetWord;
 
     /**
      * Constructor for the NetWordleGame class.
@@ -20,6 +26,7 @@ public class NetwordleGame extends Thread{
         try {
             this.client = client;
             this.cAddress = client.getLocalSocketAddress().toString();
+            this.targetWord = selectTargetWord();
 
             // Setup for Buffered Reader
             InputStreamReader in = new InputStreamReader(client.getInputStream());
@@ -29,22 +36,61 @@ public class NetwordleGame extends Thread{
             this.writer = new PrintWriter(client.getOutputStream(), true);
             
         } catch (IOException e) {
-            System.out.println("Unable to create Networdle Client." + 
-                                "Error was:\n" + e.getMessage());
+            Utils.error("Unable to create new Networdle Game.", e);
             
         }
     }
 
+    /**
+     * Main entry point for a Networdle Game. 
+     */
     public void run () {
         try {
+            // Write some generic test data. This isn't Protocol Compliant and
+            // needs to be removed before submission.
             writer.println("Welcome to the server!");
             writer.println("You are client: " + cAddress);
 
+            // Test writing something using the protocol specifications.
+            client.getOutputStream().write(ProtocolHandler.encodeMessage("START GAME"));
+            client.getOutputStream().write(ProtocolHandler.encodeMessage(targetWord));
+
         } catch (Exception e) {
-            System.out.println("An error occured for client at address: " + 
-                                cAddress);
-            System.out.println("Error was:\n" + e.getMessage());
+            Utils.error("An error occured trying to create client at: " +
+                        cAddress, e);
         }
+    }
+
+    /**
+     * Selects and returns a random word from the target words files. In the
+     * case that an error occurs trying to read the file, the generic word
+     * 'apple' will be returned so that the program can continue.
+     * @return
+     */
+    private String selectTargetWord () {
+        final String targetPath = "./resources/target.txt";
+        String target = "APPLE"; // Generic word in case reading the file fails
+
+        try {
+            int lc = 0;
+            FileReader fr = new FileReader(targetPath);
+            BufferedReader br = new BufferedReader(fr);
+
+            // Count the number of lines in the file
+            while(br.readLine() != null) lc++;
+            br.close();
+
+            // Select and read a radom line
+            Random rnd = new Random();
+            int line = rnd.nextInt(lc);
+            target = Files.readAllLines(Paths.get(targetPath)).get(line);
+
+        } catch (Exception e) {
+            Utils.error("An error has occured trying to select a target word",
+                        e);
+        }
+
+        return target; 
     }
 
 }
