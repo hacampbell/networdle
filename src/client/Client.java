d8ade79fd796e5c22bfbfea07151bcfb9672af22
Henry Campbell <hacampbell99@gmail.com>
2022-04-13 14:30:51 +1000

    Added reading and writing for the client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,15 +1,21 @@
 package src.client;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
+import src.shared.ProtocolHandler;
 import src.shared.Utils;
 
 public class Client {
 
+    private static final int MAX_BYTES = 256;
+
     public static void main(String[] args) {
         String address;
         int port;
+        boolean gameActive = true;
 
         // Check that we've been given the right arguments
         checkArgs(args);
@@ -18,10 +24,27 @@ public class Client {
         address = args[0];
         port = processPort(args[1]);
 
+        // Setup scanner to read user input.
+        Scanner input = new Scanner(System.in);
+
         // Connect to the server
         Socket connection = connectToServer(address, port);
 
-        // Disconnect when we're done
+        // Send message to start the game.
+        writeMessage("START GAME", connection);
+
+        while (gameActive) {
+            // Read response from server
+            String resp = readMessage(connection);
+            System.out.println(resp);
+
+            // Read data from client input
+            String message = input.nextLine();
+            writeMessage(message, connection);
+        }
+
+        // Disconnect when we're done, close the scanner
+        input.close();
         disconnectFromServer(connection);
     }
 
@@ -94,5 +117,39 @@ public class Client {
                 e.getMessage()
             );
         }
+    }
+
+    /**
+     * Sends a protocol compliant message to the server.
+     * @param message - The message to send to the server
+     * @param conn = The connection to the server
+     */
+    private static void writeMessage (String message, Socket conn) {
+        try {
+            OutputStream cOS = conn.getOutputStream();
+            byte[] encoded = ProtocolHandler.encodeMessage(message);
+            cOS.write(encoded);
+        } catch (IOException e) {
+            // Display the error and then close the connection.
+            Utils.error("Error sending message to serce", e );
+            disconnectFromServer(conn);
+        }  
+    }
+
+    private static String readMessage (Socket conn) {
+        byte[] resp = new byte[MAX_BYTES];
+        String msg = "";
+
+        try {
+            conn.getInputStream().read(resp);
+            msg = ProtocolHandler.decodeMessage(resp);
+        } catch (IOException e) {
+            // display the error and then close the connection
+            Utils.error("Error reading message from server.", e);
+            disconnectFromServer(conn);
+        }
+
+
+        return msg;
     }
 }
