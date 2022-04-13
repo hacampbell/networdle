045795a44333a80453b2971736da43b59f730721
Henry Campbell <hacampbell99@gmail.com>
2022-04-13 12:32:44 +1000

    Added isValidProtocolMessage

--- src/shared/ProtocolHandler.java
+++ src/shared/ProtocolHandler.java
@@ -25,4 +25,26 @@ public class ProtocolHandler {
         String str = new String(msg, StandardCharsets.US_ASCII);
         return str;
     }
+
+    /**
+     * Checks to see if a given message is valid by the rules of the networdle
+     * protocol. That is, checks to see that the message contains only ASCII
+     * characters, and that the final character is the line feed character.
+     * @param msg - The message received to be checked for validity
+     * @return - True if the message follows the protocol, otherwise false
+     */
+    public static boolean isValidProtocolMessage (byte[] msg) {
+        boolean isValid = true;
+        
+        // Check that the final character is a line feed character
+        if (msg[msg.length - 1] != 10) isValid = false;
+
+        // Make sure that, in accordance to the protocol specifications, the
+        // message contains only ASCII characters.
+        for (byte b: msg) {
+            if (b < 0 || b > 127) isValid = false;
+        }
+
+        return isValid;
+    }
 }
