294f1bc47a33beff6623f807938049d1e6f69e93
Henry Campbell <hacampbell99@gmail.com>
2022-04-14 10:12:41 +1000

    Simplified startServer.sh

--- startServer.sh
+++ startServer.sh
@@ -1,8 +1,8 @@
 echo Compiling Server...
 
-javac src/shared/Utils.java
-javac src/shared/ProtocolHandler.java
-javac src/server/Server.java
+#javac src/shared/Utils.java
+#javac src/shared/ProtocolHandler.java
+#javac src/server/Server.java
 javac src/server/Entrypoint.java
 
 echo Running Server...
